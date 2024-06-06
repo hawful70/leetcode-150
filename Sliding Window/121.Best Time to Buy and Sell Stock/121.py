957d6f4e96914291283d4892f2f223cbdcc5a2f7
Vinh Dang <vinh.dang@zoi.tech>
2024-06-06 11:10:24 +0700

    optimize 121

--- Sliding Window/121.Best Time to Buy and Sell Stock/121.py
+++ Sliding Window/121.Best Time to Buy and Sell Stock/121.py
@@ -1,16 +1,29 @@
+from typing import List
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         start = 0
         end = start + 1
+        profit = 0
 
-        res = 0
+        # while end <= len(prices) - 1:
+        #     if prices[start] > prices[end]:
+        #         start = end
+        #         end += 1
+        #     else:
+        #         current_profit = prices[end] - prices[start]
+        #         profit = max(profit, current_profit)
+        #         end += 1
         while end <= len(prices) - 1:
             if prices[start] > prices[end]:
                 start = end
                 end += 1
-            else:
-                currentProfit = prices[end] - prices[start]
-                res = max(res, currentProfit)
+            elif prices[end] - prices[start] > profit:
+                profit = prices[end] - prices[start]
                 end += 1
-        
-        return res
+            else:
+                end += 1
+        return profit
+    
+solution = Solution()
+print(solution.maxProfit([7,1,5,3,6,4]))
